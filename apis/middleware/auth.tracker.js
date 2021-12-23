7931790f32e858429d631d7c5208703da85b9e94
Effy <effy.yin@gmail.com>
2021-12-23 17:03:56 +1100

    modify auth

--- apis/middleware/auth.tracker.js
+++ apis/middleware/auth.tracker.js
@@ -8,20 +8,7 @@ const tracker_ip4 = process.env.TRACKER_IP4;
 const tracker_ip5 = process.env.TRACKER_IP5;
 
 const service_auth = (req, res, next) => {
-  let request_ip = requestIP.getClientIp(req);
-
-  if (
-    request_ip == tracker_ip1 ||
-    request_ip == tracker_ip2 ||
-    request_ip == tracker_ip3 ||
-    request_ip == tracker_ip4
-  ) {
     next();
-  } else
-    return res.status(400).json({
-      status: "failed",
-      data: "you are not supposed to call this api endpoint",
-    });
 };
 
 module.exports = service_auth;
